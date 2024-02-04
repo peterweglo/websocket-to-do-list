1f44434f27823f42f2da68b8d1b13fa418dd17b9
Piotr Węgłowski <weglowskipiotr@gmail.com>
2024-02-04 15:03:33 +0100

    Add functionality and webSocket

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
 
-app.use((req, rers) => {
+const io = socket(server);
+
+app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
+});
+
+let tasks = [];
+
+io.on('connection', (socket) => {
+  socket.emit('updateData', tasks);
+  socket.on('addTask', (task) => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+  socket.on('removeTask', (id) => {
+    tasks.filter((task) => {
+      task.id !== id;
+    });
+    socket.broadcast.emit('removeTask', id);
+  });
 });
